234e2bbbf55c33596f1d2a7be11268349de62309
ArthurWeiden <arthur.vanderweiden@student.hu.nl>
2014-04-07 15:27:27 +0200

    Unfinished Shadowtest

--- Vision_timer/ShadowFinder.cpp
+++ Vision_timer/ShadowFinder.cpp
@@ -1,25 +1,58 @@
 //Arthur van der Weiden
+//7-4-2014
 
 #include "ShadowFinder.h"
 ShadowFinder::ShadowFinder(){
+	
 }
 
-bool ShadowFinder::ShadowTest(Image& img){
-	int PixelVal = 0, NextPixelVal = 0, PrevPixelVal = 0;
-	Image destinationImage("Test.jpg", img.GetWidth(), img.GetHeight());
-
-	for (int y = 0; y <= img.GetHeight() - 1; y++){
-		for (int x = 0; x <= img.GetWidth() - 1; x++){
-			PrevPixelVal = img.GetPixel((x - 1) % img.GetWidth(), (y - 1) % img.GetHeight());
-			PixelVal = img.GetPixel(x, y);
-			NextPixelVal = img.GetPixel((x + 1) % img.GetWidth(), (y + 1) % img.GetHeight());
-			if (PrevPixelVal > PixelVal && NextPixelVal <= PixelVal && PixelVal >= 0) {
-				destinationImage.SetPixel(x, y, PixelVal);
-				return true;
+bool ShadowFinder::ShadowTest(Image& Img, int TopLeftX, int TopLeftY, int TopRightX, int TopRightY, 
+								int BottomLeftX, int BottomLeftY, int BottomRightX, int BottomRightY ){
+	
+	int LicenseplateWidth = TopRightX, LicenseplateHeight = BottomLeftY;
+	int Width = Img.GetWidth();
+	int Height = Img.GetHeight();
+	int PixelData = Img.GetPixel(TopLeftX, TopLeftY);
+	int GrayValue, PrevPrevGrayValue, PrevGrayValue, NextGrayValue;
+	int totalPixels = LicenseplateHeight * LicenseplateWidth;
+	Image outputImage("Test.jpg", Width, Height);
+	int i = 0;
+	for (int y = TopLeftY ; y <= LicenseplateHeight; y++){
+		for (int x = TopLeftX + 2; x <= LicenseplateWidth; x++){
+			outputImage.SetPixel(x, y, Img.GetPixel(x, y));
+			PrevPrevGrayValue = (int)((Img.GetPixelRed((x - 2) % LicenseplateWidth, y) * 0.30)
+				+ (Img.GetPixelGreen((x - 2) % LicenseplateWidth, y) * 0.59)
+				+ (Img.GetPixelBlue((x - 2) % LicenseplateWidth, y) * 0.11));
+			PrevGrayValue = (int)((Img.GetPixelRed((x - 1) % LicenseplateWidth, y) * 0.30)
+				+ (Img.GetPixelGreen((x - 1) % LicenseplateWidth, y) * 0.59)
+				+ (Img.GetPixelBlue((x - 1) % LicenseplateWidth, y) * 0.11));
+			GrayValue = (int)((Img.GetPixelRed(x, y) * 0.30) + (Img.GetPixelGreen(x, y) * 0.59) + (Img.GetPixelBlue(x, y) * 0.11));
+			NextGrayValue = (int)((Img.GetPixelRed((x + 1) % LicenseplateWidth, y) * 0.30)
+				+ (Img.GetPixelGreen((x + 1) % LicenseplateWidth, y) * 0.59)
+				+ (Img.GetPixelBlue((x + 1) % LicenseplateWidth, y) * 0.11));
+			if (NextGrayValue < BLACKGRAY){
+				if ( abs(PrevPrevGrayValue - PrevGrayValue) < SHADOWMARGE){
+					if (abs(GrayValue - PrevGrayValue) > SHADOWMARGE && abs(NextGrayValue - GrayValue) < SHADOWMARGE){
+						i++;
+						outputImage.SetPixel(x, y, (255 << redPixelShift) | (0 << greenPixelShift) | (0 << bluePixelShift));
+					}
+				}
 			}
+		PixelData = Img.GetPixel(x, y);
 		}
-		
+	}	
+	std::cout << i << std::endl;
+	std::cout << totalPixels << std::endl;
+
+	outputImage.SaveImageToFile("OUTPUT_");
+	if (i < totalPixels / totalPixels * 2){
+		std::cout << "NEEEEE" << std::endl;
+		return false;
 	}
-	return false;
-	destinationImage.SaveImageToFile("OUTPUT_");
+	else {
+		std::cout << "JAAAAA" << std::endl;
+		return true;
+	}
+
+	
 }
