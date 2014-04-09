249e9433e0d60da78b6c37d760c308d501a7ac7c
hendrik386 <hendrik.cornelisse@student.hu.nl>
2014-04-09 16:46:28 +0200

    overexposer_fix_beta

--- OverexposureTest/OverexposureTest/test.cpp
+++ OverexposureTest/OverexposureTest/test.cpp
@@ -1,5 +1,5 @@
 #include "BaseTimer.h"
-#include "Alg.h"
+#include "Overexposure_Test.h"
 
 #include <ImageLoader.h>
 #include <iostream>
@@ -14,7 +14,8 @@ int main(){
 	shared_ptr<ImageRGB> img = loadImg("license_plate_8.bmp");
 
 	bt.start();
-	cout << OT.Overexposure_Detection(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630) << '\n';
+	//cout << OT.Overexposure_Detection(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630) << '\n';
+	OT.Overexposure_gone(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630);
 	bt.stop();
 
 	cout << bt.elapsedSeconds() << '\n';
