0df94945cfff644b83b9c4adcabc2ef9a485d9f1
ArthurWeiden <arthur.vanderweiden@student.hu.nl>
2014-04-09 12:22:07 +0200

    Shadow_detection FINAL

--- ShadowTest/ShadowTest/Main.cpp
+++ ShadowTest/ShadowTest/Main.cpp
@@ -1,34 +1,37 @@
-
 #include <iostream>
-#include <fstream>
-#include <array>
-#include <numeric>
 #include <ImageLoader.h>
-#include <memory>
 #include "ShadowTest.h"
-#include <stdlib.h>
+#include "BaseTimer.h"
+
 using namespace ImageLib;
 using namespace std;
 
 int main(){	
 	ShadowTest shadow;
-	//std::cout << shadow.ShadowSearch("license_plate_1.jpg", 298, 39, 1441, 236);
-	//std::cout << shadow.ShadowSearch("license_plate_2.jpg", 659, 701, 992, 778);
-	//std::cout << shadow.ShadowSearch("license_plate_3.jpg", 341, 354, 562, 410);
-	//std::cout << shadow.ShadowSearch("license_plate_4.jpg", 928, 582, 1087, 614);
-	//std::cout << shadow.ShadowSearch("license_plate_5.jpg", 212, 281, 382, 318);
-	//std::cout << shadow.ShadowSearch("license_plate_6.jpg", 1377, 1473, 2224, 1509);
-	//std::cout << shadow.ShadowSearch("license_plate_7.jpg", 398, 388, 1193, 537);
-	//std::cout << shadow.ShadowSearch("license_plate_8.jpg", 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630);
-	//std::cout << shadow.ShadowSearch("license_plate_9.jpg", 108, 191, 698, 235);
-	//std::cout << shadow.ShadowSearch("license_plate_10.jpg", 718, 802, 1847, 964);
-	//std::cout << shadow.ShadowSearch("license_plate_11.jpg", 656, 1326, 1001, 1402);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_2.jpg", 757, 634, 895, 677);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_3.jpg", 2610, 1237, 2900, 1300);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_4.png", 63, 6, 563, 122);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_5.jpg", 145, 145, 605, 235);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_6.jpg", 187, 213, 265, 230);
-	//std::cout << shadow.ShadowSearch("license_plate_ex_7.jpg", 129, 185, 208, 208);
-	system("Output.jpg");
-	std::cin.ignore();
+	shared_ptr<ImageRGB> TestImage = loadImg("license_plate_8.jpg");
+	BaseTimer bt;
+	bt.start();
+	cout << shadow.Shadow_Detection(TestImage, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630) << endl;
+	//cout << shadow.Shadow_Detection(TestImage, 145, 145, 605, 145, 155, 240, 605, 235) << endl;
+	bt.stop();
+	cout << bt.elapsedSeconds() << endl;
+	cin.ignore();
 }
+
+//cout << shadow.ShadowSearch("license_plate_1.jpg",	298, 39, 1441, 236);
+//cout << shadow.ShadowSearch("license_plate_2.jpg",	659, 701, 992, 778);
+//cout << shadow.ShadowSearch("license_plate_3.jpg",	341, 354, 562, 410);
+//cout << shadow.ShadowSearch("license_plate_4.jpg",	928, 582, 1087, 614);
+//cout << shadow.ShadowSearch("license_plate_5.jpg",	212, 281, 382, 318);
+//cout << shadow.ShadowSearch("license_plate_6.jpg",	1377, 1473, 2224, 1509);
+//cout << shadow.ShadowSearch("license_plate_7.jpg",	398, 388, 1193, 537);
+//cout << shadow.ShadowSearch("license_plate_8.jpg",	1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630);
+//cout << shadow.ShadowSearch("license_plate_9.jpg",	108, 191, 698, 235);
+//cout << shadow.ShadowSearch("license_plate_10.jpg",	718, 802, 1847, 964);
+//cout << shadow.ShadowSearch("license_plate_11.jpg",	656, 1326, 1001, 1402);
+//cout << shadow.ShadowSearch("license_plate_ex_2.jpg", 757, 634, 895, 677);
+//cout << shadow.ShadowSearch("license_plate_ex_3.jpg", 2610, 1237, 2900, 1300);
+//cout << shadow.ShadowSearch("license_plate_ex_4.png", 63, 6, 563, 122);
+//cout << shadow.ShadowSearch("license_plate_ex_5.jpg", 145, 145, 605, 235);
+//cout << shadow.ShadowSearch("license_plate_ex_6.jpg", 187, 213, 265, 230);
+//cout << shadow.ShadowSearch("license_plate_ex_7.jpg", 129, 185, 208, 208);
--- ShadowTest/ShadowTest/ShadowTest.cpp
+++ ShadowTest/ShadowTest/ShadowTest.cpp
@@ -2,35 +2,35 @@
 
 #include "ShadowTest.h"
 
-bool ShadowTest::ShadowSearch(std::string FileName, int TopLeftX, int TopLeftY, int TopRightX, int TopRightY,
+bool ShadowTest::Shadow_Detection(std::shared_ptr<ImageRGB> img, int TopLeftX, int TopLeftY, int TopRightX, int TopRightY,
 								int BottomLeftX, int BottomLeftY, int BottomRightX, int BottomRightY){
+
 	int TopBigY, TopSmallY, BottomBigY, BottomSmallY;
 
 	if (TopLeftY < TopRightY){
 		TopBigY = TopRightY;
 		TopSmallY = TopLeftY;
-	}
-	else {
+	}else {
 		TopBigY = TopLeftY;
 		TopSmallY = TopRightY;
-	}if (BottomLeftY < BottomRightY){
+	}
+	if (BottomLeftY < BottomRightY){
 		BottomBigY = BottomRightY;
 		BottomSmallY = BottomLeftY;
-	}
-	else {
+	}else {
 		BottomBigY = BottomLeftY;
 		BottomSmallY = BottomRightY;
 	}
 
 	int LicensePlateWidth = BottomRightX;
-	int LicensePlateHeight = BottomBigY;
+	int LicensePlateHeight = BottomBigY - TopSmallY;
 
 	int TotalPixels = LicensePlateHeight * LicensePlateWidth;
-	std::unique_ptr<ImageRGB> TestImage = loadImg(FileName);
-	auto rgb_ptrs = TestImage->data(TopLeftX + 1, TopLeftY + 1);
-	auto Grayval = (*rgb_ptrs.red * 0.21) + (*rgb_ptrs.blue * 0.71) + (*rgb_ptrs.green * 0.07);
+	float percentage;
 	int ShadowPixels = 0;
-	auto Darkest = (*rgb_ptrs.red * 0.21) + (*rgb_ptrs.blue * 0.71) + (*rgb_ptrs.green * 0.07);
+	auto rgb_ptrs = img->data(TopLeftX + 1, TopLeftY + 1);
+	auto Grayval = (*rgb_ptrs.red * 0.21) + (*rgb_ptrs.blue * 0.71) + (*rgb_ptrs.green * 0.07);
+	auto Darkest = Grayval;
 	
 	for (int x = TopLeftX + 1; x <= LicensePlateWidth; x++){
 		for (int y = TopSmallY + 1; y <= BottomBigY; y++){
@@ -45,30 +45,18 @@ bool ShadowTest::ShadowSearch(std::string FileName, int TopLeftX, int TopLeftY, 
 		for (int y = TopSmallY + 1; y <= BottomBigY; y++){
 			Grayval = (*rgb_ptrs.red * 0.21) + (*rgb_ptrs.blue * 0.71) + (*rgb_ptrs.green * 0.07);
 
-			rgb_ptrs = TestImage->data(x, y);
 			rgb_ptrs.red++;
 			rgb_ptrs.blue++;
 			rgb_ptrs.green++;	
-			TestImage->at(x, y).blue = Grayval;
-			TestImage->at(x, y).green = Grayval;
-			TestImage->at(x, y).red = Grayval;
 			
-			if ((rgb_ptrs.blue > rgb_ptrs.green && rgb_ptrs.blue > rgb_ptrs.red) && (Grayval >= Darkest && Grayval <= 60)){
+			if ((rgb_ptrs.blue > rgb_ptrs.green && rgb_ptrs.blue > rgb_ptrs.red) && (Grayval >= Darkest && Grayval <= YELLOWGRAY)){
 				ShadowPixels++;
-				TestImage->at(x, y).blue = 0;
-				TestImage->at(x, y).green = 0;
-				TestImage->at(x, y).red = 255;
+			}
+			percentage = ((float)ShadowPixels / (float)TotalPixels) * 100;
+			if (percentage > 2.0f){
+				FoundShadow = true;
 			}
 		}
 	}
-
-	float percentage = ((float)ShadowPixels / (float)TotalPixels) * 100;
-	if (percentage > 2.0f){
-		FoundShadow = true;
-	}
-	std::cout << TopSmallY << std::endl;
-	std::cout << BottomBigY << std::endl;
-	std::cout << percentage << std::endl;
-	saveImg(*TestImage, "Output.jpg");
 	return FoundShadow;
 }
