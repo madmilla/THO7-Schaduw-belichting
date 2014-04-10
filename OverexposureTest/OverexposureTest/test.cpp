fe71350da68be8bdf9e4791f49a1d487031da96c
hendrik386 <hendrik.cornelisse@student.hu.nl>
2014-04-10 16:34:45 +0200

    Overexposure_remove

--- OverexposureTest/OverexposureTest/test.cpp
+++ OverexposureTest/OverexposureTest/test.cpp
@@ -9,13 +9,15 @@ using namespace ImageLib;
 
 int main(){
 	BaseTimer bt;
-	Overexposure_Test OT;
 
-	shared_ptr<ImageRGB> img = loadImg("license_plate_8.bmp");
+
+	shared_ptr<ImageRGB> img = loadImg("license_plate_8.jpg");
 
 	bt.start();
+	Overexposure_Test OT;
 	//cout << OT.Overexposure_Detection(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630) << '\n';
-	OT.Overexposure_gone(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630);
+	OT.Overexposure_Removal(img, 1796, 1518, 2562, 1447, 1815, 1692, 2580, 1630);
+
 	bt.stop();
 
 	cout << bt.elapsedSeconds() << '\n';
